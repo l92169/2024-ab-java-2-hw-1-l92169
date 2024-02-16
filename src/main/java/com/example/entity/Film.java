ff4294d8861402e0604540ef826f98d7904e0c7f
elizaveta <l1zail@yandex.ru>
2024-02-16 17:41:37 +0300

    change strategy

--- src/main/java/com/example/entity/Film.java
+++ src/main/java/com/example/entity/Film.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Film {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
